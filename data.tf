6a9c97163b791474be0a6c4f581262a7b535c246
Yong Wen Chua <lawliet89@users.noreply.github.com>
2023-01-18 09:12:44 +0800

    Fix Projects Support (#4)

--- data.tf
+++ data.tf
@@ -1,9 +1,12 @@
 locals {
-  tfc_workload_identity_workspaces = flatten([
+  tfc_workload_identity_workspaces = distinct(flatten([
     for org, workspaces in var.tfc_workload_identity_workspaces : [
-      for workspace in workspaces : "organization:${org}:workspace:${workspace}:run_phase:*"
+      for workspace in workspaces : [
+        "organization:${org}:workspace:${workspace}:run_phase:*",
+        "organization:${org}:project:${var.tfc_project_support_match}:workspace:${workspace}:run_phase:*",
+      ]
     ]
-  ])
+  ]))
 
   oidc_provider_url = "https://app.terraform.io"
 }
