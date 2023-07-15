43658942f4742bd5baa7e5c4b6899e18342ceaf3
Yong Wen Chua <lawliet89@users.noreply.github.com>
2023-07-15 09:41:44 +0800

    Fix

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_openid_connect_provider" "tfc_provider" {
   url            = local.oidc_provider_url
   client_id_list = var.tfc_oidc_provider_audiences
   thumbprint_list = [
-    data.tls_certificate.tfc_certificate.certificates.0.sha1_fingerprint,
+    data.tls_certificate.tfc_certificate.certificates[0].sha1_fingerprint,
   ]
 }
 
